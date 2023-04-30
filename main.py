b5233a8f08645cbcb23902d69d48cedf707167f7
swarali883 <112435016+swarali883@users.noreply.github.com>
2023-04-30 19:56:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import smtplib
 import time
 
-MY_EMAIL = "swarali83@yahoo.com"
-MY_PASSWORD = "11!!yahoO"
+MY_EMAIL = "email address"
+MY_PASSWORD = "password"
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
 
